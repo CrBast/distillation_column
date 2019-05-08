bd6af4ef83efd1f7f0b9adc38400375d24fc5522
Bastien <bastien.crettenand@hotmail.com>
2019-05-08 22:12:17 +0200

    Add methods for implementation

--- arduino/components_test/lcd/lcd.ino
+++ arduino/components_test/lcd/lcd.ino
@@ -8,17 +8,23 @@ Arduino LDC test
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+int line = 0;
+
 void setup() {
-	// set up the LCD's number of columns and rows:
 	lcd.begin(16, 2);
-	// Print a message to the LCD.
-	lcd.print("hello, world!");
 }
 
 void loop() {
-	// set the cursor to column 0, line 1
-	// (note: line 1 is the second row, since counting begins with 0):
-	lcd.setCursor(0, 1);
-	// print the number of seconds since reset:
-	lcd.print(millis() / 1000);
+	line++;
+	if (line > 1) {
+		line = 0;
+	}
+	int temp = millis() / 1000;
+	ldcSetTextByLine(line, (String)temp);
+	delay(1000);
+}
+
+void ldcSetTextByLine(int line, String content) {
+	lcd.setCursor(0, line);
+	lcd.print(content);
 }
